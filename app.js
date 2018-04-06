eff8f5330663c6546af391dd8e5c734b9e235f48
MARIO NISHIO <mario.nishio@gmail.com>
2018-04-06 13:49:16 -0700

    Finished Q3

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +50,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var firstSum = sum(a, b).shift();
+  var finalSum = sum(firstSum, c).shift();
+  var firstPruduct = multiply(a, b).shift();
+  var finalProduct = multiply(firstPruduct, c).shift();
+  return [finalSum, finalProduct, a + ' and ' + b + ' and ' + c + ' sum to ' + finalSum + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + finalProduct + '.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
